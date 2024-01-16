64afee538edd26361650b269059e3b34893a4f65
Emerson Thiago <emersonthiago168@outlook.com>
2024-01-16 19:47:45 -0300

    registrando usuário no sistema

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -1,4 +1,5 @@
 const User = require('../models/User');
+const bcrypt = require('bcrypt');
 
 module.exports = class UserController {
     static async register(req, res) {
@@ -38,9 +39,28 @@ module.exports = class UserController {
         // check if user exists
         const userExists = await User.findOne({ email });
 
-        if(userExists) {
-            res.status(422).json({message: 'Por favor, utilize outro e-mail!'});
+        if (userExists) {
+            res.status(422).json({ message: 'Por favor, utilize outro e-mail!' });
             return;
+        }
+
+        // create a password
+        const salt = await bcrypt.genSalt(12);
+        const passwordHash = await bcrypt.hash(password, salt);
+
+        // create a user 
+        const user = new User({
+            name,
+            email,
+            phone,
+            password: passwordHash
+        });
+
+        try {
+            const newUser = await user.save();
+            res.status(201).json({message: 'Usuário criado', newUser});
+        } catch (err) {
+            res.status(500).json({ message: err});
         }
     }
 }
