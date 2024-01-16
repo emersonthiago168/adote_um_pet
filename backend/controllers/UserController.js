57b2b2644684815a1d7ca4f1c7f36a240b9cff21
Emerson Thiago <emersonthiago168@outlook.com>
2024-01-16 20:27:00 -0300

    função de login no sistema

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -59,10 +59,44 @@ module.exports = class UserController {
 
         try {
             const newUser = await user.save();
-            
+
             await createUserToken(newUser, req, res)
         } catch (err) {
-            res.status(500).json({ message: err});
+            res.status(500).json({ message: err });
         }
+    }
+
+    static async login(req, res) {
+        const { email, password } = req.body;
+
+        if (!email) {
+            res.status(422).json({ message: 'O e-mail é obrigatório' });
+            return;
+        }
+
+        if (!password) {
+            res.status(422).json({ message: 'A senha é obrigatória' });
+            return;
+        }
+
+        // check if user exists
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            res.status(422).json({
+                message: 'Não há usuário cadastro com este e-mail!'
+            })
+            return;
+        }
+
+        // check if password match with db password
+        const checkPassword = await bcrypt.compare(password, user.password);
+
+        if (!checkPassword) {
+            res.status(422).json({ message: 'Senha inválida!'});
+        }
+
+        await createUserToken(user, req, res);
+
     }
 }
