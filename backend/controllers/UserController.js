33f74b2cea42e26ca3452e3c5702680aa0eab544
Emerson Thiago <emersonthiago168@outlook.com>
2024-01-17 19:37:25 -0300

    resgatando usuário por id

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -106,16 +106,29 @@ module.exports = class UserController {
     static async checkUser(req, res) {
         let currentUser;
 
-        if(req.headers.authorization) {
+        if (req.headers.authorization) {
             const token = getToken(req);
             const decoded = jwt.verify(token, 'meusecret');
 
             currentUser = await User.findById(decoded.id);
-            currentUser.password = undefined; 
+            currentUser.password = undefined;
         } else {
             currentUser = null;
         }
 
         res.status(200).send(currentUser);
+    }
+
+    static async getUserById(req, res) {
+        const id = req.params.id;
+
+        const user = await User.findById(id).select('-password');
+
+        if (!user) {
+            res.status(422).json({ message: 'Usuário não encontrado!' });
+            return;
+        }
+
+        res.status(200).json(user);
     }
 }
