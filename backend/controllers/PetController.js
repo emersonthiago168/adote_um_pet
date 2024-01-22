458cb72f02a7385757e36aecdc747802e3a9a9a0
Emerson Thiago <emersonthiago168@outlook.com>
2024-01-22 15:33:26 -0300

    salvando pet no sistema

--- backend/controllers/PetController.js
+++ backend/controllers/PetController.js
@@ -1,7 +1,45 @@
 const Pet = require('../models/Pet');
 
+// helpers
+const getToken = require('../helpers/get-token');
+const getUserByToken = require('../helpers/get-user-by-token');
+
 module.exports = class PetController {
     static async create(req, res) {
-        res.json({ message: 'Deu certo!' });
+        const { name, age, weight, color } = req.body;
+
+        const available = true;
+
+        // images upload
+
+        // validations
+        if (!name) return res.status(422).json({ message: 'O nome é obrigatório!' });
+
+        if (!age) return res.status(422).json({ message: 'A idade é obrigatória!' });
+
+        if (!weight) return res.status(422).json({ message: 'O peso é obrigatório!' });
+
+        if (!color) return res.status(422).json({ message: 'A cor é obrigatória!' });
+
+        // get pet owner
+        const token = getToken(req);
+        const user = await getUserByToken(token);
+
+        // create a pet
+        const pet = new Pet({
+            name, age, weight, color, available, images: [], user: {
+                _id: user._id,
+                name: user.name,
+                image: user.image,
+                phone: user.phone
+            }
+        });
+
+        try {
+            const newPet = await pet.save();
+            res.status(201).json({ message: 'Pet cadastrado com sucesso!', newPet });
+        } catch (err) {
+            res.status(500).json({ message: err });
+        }
     }
 }
