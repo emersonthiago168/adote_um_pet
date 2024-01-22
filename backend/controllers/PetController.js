9893d4a1b65127af64470cb29a27381c757ff792
Emerson Thiago <emersonthiago168@outlook.com>
2024-01-22 16:00:15 -0300

    função de resgatar todos os pets

--- backend/controllers/PetController.js
+++ backend/controllers/PetController.js
@@ -48,4 +48,10 @@ module.exports = class PetController {
             res.status(500).json({ message: err });
         }
     }
+
+    static async getAll(req, res) {
+        const pets = await Pet.find().sort('-createdAt');
+
+        res.status(200).json({ pets });
+    }
 }
